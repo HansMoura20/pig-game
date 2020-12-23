64dcb5cb4ad3b12e5d87f138ca2d54bdb479006d
HansMoura20 <hansadrienmoura@outlook.com>
2020-12-23 13:59:27 +0800

    display current score

--- script.js
+++ script.js
@@ -17,14 +17,24 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
+let currentScore = 0;
+
 //Rolling dice functionality
 btnRoll.addEventListener('click', function () {
   //1. Generating a random dice roll
   const dice = Math.floor(Math.random() * 6) + 1;
+  console.log(dice);
+
   //2. Display dice;
   diceEl.classList.remove('hidden');
   diceEl.src = `dice-${dice}.png`;
-  console.log(dice);
+
   //3. Check for rolled 1: if true, switch player
-  
+  if(dice !== 1){
+    //Add dice to current score
+    currentScore += dice;
+    current0El.textContent = currentScore;
+  }else{
+    //Switch to next player
+  }
 });
