4e8256a30913fbb9789f45089ad1c9dade4412a9
HansMoura20 <hansadrienmoura@outlook.com>
2020-12-23 14:20:04 +0800

    switching active player

--- script.js
+++ script.js
@@ -17,7 +17,11 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
+
+
 
 //Rolling dice functionality
 btnRoll.addEventListener('click', function () {
@@ -30,11 +34,18 @@ btnRoll.addEventListener('click', function () {
   diceEl.src = `dice-${dice}.png`;
 
   //3. Check for rolled 1: if true, switch player
-  if(dice !== 1){
+  if (dice !== 1) {
     //Add dice to current score
     currentScore += dice;
-    current0El.textContent = currentScore;
-  }else{
+    document.querySelector(
+      `#current--${activePlayer}`
+    ).textContent = currentScore;
+  } else {
     //Switch to next player
+    document.querySelector(`#current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
   }
 });
