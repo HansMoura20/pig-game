2c3c4314248ac6c93200d62f2b6829a75b38f6f5
HansMoura20 <hansadrienmoura@outlook.com>
2020-12-23 13:54:19 +0800

    display dice depending on random number

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 'use strict';
+
+//selecting elements
+const score0El = document.querySelector('#score--0');
+const score1El = document.querySelector('#score--1');
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+const current0El = document.querySelector('#current--0');
+const current1El = document.querySelector('#current--1');
+
+//starting conditions
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
+
+//Rolling dice functionality
+btnRoll.addEventListener('click', function () {
+  //1. Generating a random dice roll
+  const dice = Math.floor(Math.random() * 6) + 1;
+  //2. Display dice;
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+  console.log(dice);
+  //3. Check for rolled 1: if true, switch player
+  
+});
